76f9f7f94c96ad3a85d93af84053d49cc93e550f
“AratiG“ <aratinalla@gmail.com>
2023-05-18 21:11:55 +0530

    Added: Code to print percentage of head and tail after flip the coin

--- src/basiccoreprograms/BasicCorePrograms.java
+++ src/basiccoreprograms/BasicCorePrograms.java
@@ -1,8 +1,28 @@
 package basiccoreprograms;
 
+import java.util.Scanner;
+
 public class BasicCorePrograms {
     public static void main(String[] args) {
         System.out.println("Welcome to Basic Core Programs...");
+        flipCoin();
     }
 
+    public static void flipCoin() {
+        int head = 0;
+        int tail = 0;
+        double randomnum = 0.0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number upto which you want to flip the coin :");
+        int num = sc.nextInt();
+        for (int i = 1; i <= num; i++) {
+            randomnum = Math.random();
+            if (randomnum < 0.5)
+                tail++;
+            else
+                head++;
+        }
+        System.out.println("count of head :" + head);
+        System.out.println("count of tail :" + tail);
+    }
 }
