42055739b8a06a8fa895876677f7ed0ffb520d7b
“AratiG“ <aratinalla@gmail.com>
2023-05-18 21:08:33 +0530

    Added: Basic core programs class

---
+++ src/basiccoreprograms/BasicCorePrograms.java
@@ -0,0 +1,8 @@
+package basiccoreprograms;
+
+public class BasicCorePrograms {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Basic Core Programs...");
+    }
+
+}
